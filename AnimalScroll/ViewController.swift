7f8f859f7a3d3ef10c5ac49ada9ebbd38329e587
Virginia <virlli_nia@hotmail.com>
2019-05-15 20:11:19 +0200

    Añadimos el protocolo Asociamos el delegado del ScrollView a ViewController Conectamos outlets de ScrollView y sus constrainsts (top, bottom, trailing, landing)

--- AnimalScroll/ViewController.swift
+++ AnimalScroll/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imagen: UIImageView!
+    @IBOutlet weak var scrollImage: UIScrollView!
+
+
+    @IBOutlet weak var leadingCons: NSLayoutConstraint!
+    @IBOutlet weak var bottomCons: NSLayoutConstraint!
+    @IBOutlet weak var topCons: NSLayoutConstraint!
+
+    @IBOutlet weak var trailingCons: NSLayoutConstraint!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+
+
+
+
+}
+extension ViewController: UIScrollViewDelegate {
+
+
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imagen
+    }
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        debugPrint("Scroll")
+    }
 
 }
 

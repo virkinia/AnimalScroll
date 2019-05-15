e54b7f97cd0df01aa4c6ca6a1b008c9d491dc1da
Virginia <virlli_nia@hotmail.com>
2019-05-15 17:40:47 +0200

    Initial Commit

---
+++ AnimalScroll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimalScroll
+//
+//  Created by Dev2 on 15/05/2019.
+//  Copyright © 2019 CFTIC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
